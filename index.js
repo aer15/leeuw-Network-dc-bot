0a320ab6a6cafa17a74837cb990d9632cf8b010a
aer15 <arne.bosmans@gmail.com>
2021-09-19 16:19:24 +0200

    8
    8

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ client.login(botconfig.token);
 
 client.on("ready", async () => {
     
+  console.log(`${client.user.username} is online.`);
+  client.user.setActivity("testing", {type: "faze"});
+  
+});
+
+client.on("ready", async () => {
+    
     console.log(`${client.user.username} is online.`);
     client.user.setActivity("testing", {type: "faze"});
     
